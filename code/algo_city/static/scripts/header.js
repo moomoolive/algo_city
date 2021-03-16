c8eaad2bac940e50074b828eb00b788deb31ca53
Mostafa Elbannan <mostafaelbannan1@gmail.com>
2021-03-15 20:15:40 -0600

    finished app header

--- code/algo_city/static/scripts/header.js
+++ code/algo_city/static/scripts/header.js
@@ -1,13 +1,43 @@
-var headerButton = {
-    element: window.document.getElementById("menu-button"),
-    isActive: false
-}
-
-function toggleNavIcon(isActive) {
-    return isActive ?  '📘' : '📖'
+var header = {
+    button: window.document.getElementById("menu-button"),
+    dropdownMenu: window.document.getElementById("dropdown-menu"),
+    showDropdownMenu: false
 }
 
 function toggleNavBar() {
-    headerButton.element.innerHTML = toggleNavIcon(headerButton.isActive) + ' Menu'
-    headerButton.isActive = !headerButton.isActive
+    var toggleHeaderVisibility = function () {
+        header.showDropdownMenu = !header.showDropdownMenu
+    }
+    var toggleNavIcon = function (isActive) {
+        var icon = '📘'
+        if (isActive) {
+            icon = '📖'
+        }
+        var text = icon + ' Menu'
+        header.button.innerHTML = text
+    }
+    var togglePressedBoxShadow = function (isActive) {
+        var className = 'pressed'
+        if (isActive) {
+            header.button.classList.add(className)
+        } else {
+            header.button.classList.remove(className)
+        }
+    }
+    var toggleDropdownMenuVisibility = function (isActive) {
+        header.dropdownMenu.style.display = isActive ? 'block' : 'none'
+    }
+
+    toggleHeaderVisibility()
+    toggleNavIcon(header.showDropdownMenu)
+    togglePressedBoxShadow(header.showDropdownMenu)
+    toggleDropdownMenuVisibility(header.showDropdownMenu)
+}
+
+function toSorting() {
+    window.location.href = '/sorting'
+}
+
+function toDataStructures() {
+    window.location.href = '/data-structures'
 }
